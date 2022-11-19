18aa63435b48310cc586c62746b30bd539b25cb8
AnKlvy <andreyklimov.ver2@gmail.com>
2022-11-19 21:22:58 +0600

    start

---
+++ src/main/java/com/vsk2shablon/Vsk2shablonApplication.java
@@ -0,0 +1,13 @@
+package com.vsk2shablon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vsk2shablonApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Vsk2shablonApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/vsk2shablon/Vsk2shablonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsk2shablon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vsk2shablonApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
